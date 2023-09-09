019a3b4a9aee399f631d6d62664db3305df1f73c
MyatMinAung <myatminaung.dev@gmail.com>
2023-09-09 22:55:25 +0630

    wip: sql database

--- db.sql
+++ db.sql
@@ -1,32 +1,17 @@
-/* Create */
-CREATE DATABASE db_name;
-
-/* Use */
-USE db_name;
-
-/* Drop */
-DROP DATABASE db_name;  
-
-/**
- * Read Only
- * 1 = true, 0 = false
+/* 
+ * Relational Database Management System (RDMS)
+ * Defines database relationships in the form of tables
+ * The tables are related to each other - based on data common to each.
+ * SQL keywords are NOT case sensitive
  */
-ALTER DATABASE db_name READ ONLY = 1;
 
-/**
- * Used to specify rules for data in a table.
- */
-CONSTRAINT table_name PRIMARY KEY (column_name);
-
-/**
- * Uniquely identifies each record in a table.
- * Must contain UNIQUE values, and cannot contain NULL values. 
- * 
- */
-CREATE TABLE employees (
-    id INT NOT NULL,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    age INT,
-    PRIMARY KEY (id)
-)
+/* Most Important SQL commands */
+UPDATE - updates data in a database
+DELETE - deletes data from a database
+INSERT INTO - inserts new data into a database
+CREATE DATABASE - creates a new database
+ALTER DATABASE - modifies a database
+ALTER TABLE - modifies a table
+DROP TABLE - deletes a table
+CREATE INDEX - creates an index (search key)
+DROP INDEX - deletes an index
